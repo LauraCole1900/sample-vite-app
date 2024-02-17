b91ee63d6f4dc403fa413b3cd52c7a88ff286631
Laura Cole <lauracole1900@comcast.net>
2024-02-16 20:52:44 -0700

    added gh-pages, added base to Router and vite.config, adjusted scripts in package.json, defined my output folder in vite.config, added my routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
+import { Contact, Home, ProjectGallery } from './components/Pages';
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <Router basename={'/sample-vite-app'}>
       <MyNav />
-    </>
+      <Routes>
+        <Route path='/portfolio' element={<ProjectGallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
