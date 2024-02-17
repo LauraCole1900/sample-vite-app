ee6f7db902bdafe0fc045a77abb978010c588d37
Laura Cole <lauracole1900@comcast.net>
2024-02-16 22:05:01 -0700

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
 import { Contact, Custom404, Home, ProjectGallery } from './components/Pages';
 import './App.css'
