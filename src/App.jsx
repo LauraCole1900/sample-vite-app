6a019b96d9fb23360cc55573f5d2cad6f081c2e5
Laura Cole <lauracole1900@comcast.net>
2024-02-16 21:57:57 -0700

    adjusted basename to include the github base as well as the repo to see if that fixes my routing problems

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
 
   return (
-    <Router basename={`./`}>
+    <Router basename={`lauracole1900.github.io/sample-vite-app/`}>
       <MyNav />
       <Routes>
         <Route path='/portfolio' element={<ProjectGallery />} />
