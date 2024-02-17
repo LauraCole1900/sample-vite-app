c7530868dd1e1e95c4b8117fa8e0a5142e8986f9
Laura Cole <lauracole1900@comcast.net>
2024-02-16 21:43:07 -0700

    took leading / out of routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
     <Router basename={`/sample-vite-app`}>
       <MyNav />
       <Routes>
-        <Route path='/portfolio' element={<ProjectGallery />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/404' element={<Custom404 />} />
+        <Route path='portfolio' element={<ProjectGallery />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='404' element={<Custom404 />} />
         <Route path='' element={<Home />} />
       </Routes>
     </Router>
