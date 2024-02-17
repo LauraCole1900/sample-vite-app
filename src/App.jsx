5ff3de1b6c023b93208b9a08525352cbd325f4a1
Laura Cole <lauracole1900@comcast.net>
2024-02-16 22:01:53 -0700

    fussing with basename and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
 
   return (
-    <Router basename={`lauracole1900.github.io/sample-vite-app/`}>
+    <Router basename={`/sample-vite-app`}>
       <MyNav />
       <Routes>
         <Route path='/portfolio' element={<ProjectGallery />} />
