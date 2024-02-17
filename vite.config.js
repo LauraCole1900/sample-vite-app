6cd6cf53924d147b491f50cf83acfc10d12c7ba5
Laura Cole <lauracole1900@comcast.net>
2024-02-16 21:35:42 -0700

    still trying to get the repo name into the routes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-sample-app',
+  base: '/sample-vite-app/',
   build: {
     outDir: 'build'
   },
