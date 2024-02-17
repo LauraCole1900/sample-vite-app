52d8e88a929e4b3c373e2ba111efd3e57a61d6da
Laura Cole <lauracole1900@comcast.net>
2024-02-16 20:56:44 -0700

    added 404 page and route

---
+++ src/components/Pages/404.jsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+  return (
+    <>
+    <h1>404 You've reached the undiscovered country! Please use your back button to go back.</h1>
+    </>
+  )
+}
+
+export default Custom404;
